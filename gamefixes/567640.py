9b3d732d42b743313047b12320df42e89a3d7abb
pierobot <pierobot@users.noreply.github.com>
2020-02-12 21:22:51 -0600

    Add gamefix for Danganronpa V3

---
+++ gamefixes/567640.py
@@ -0,0 +1,11 @@
+""" Danganronpa V3: Killing Harmony
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Requires media foundation dlls
+    """
+
+    util.protontricks('mf_install')
